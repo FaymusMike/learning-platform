e6c1118aba44becfb1fd9c2642176b175bc7495a
FaymusMike <fayosmichael@gmail.com>
2025-11-07 17:42:05 +0100

    update

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,16 @@
-// Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id"
+  apiKey: "AIzaSyAZQrPWExzsRK8Use3QLrt0CKPxUgsxmes",
+  authDomain: "blockvote-bf739.firebaseapp.com",
+  projectId: "blockvote-bf739",
+  storageBucket: "blockvote-bf739.firebasestorage.app",
+  messagingSenderId: "801871592271",
+  appId: "1:801871592271:web:db3c8768ea41b52ccbe33a"
 };
 
 // Initialize Firebase
